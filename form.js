d464f532218e1750d2939f9767dfc8251b25e7fb
magnifiq <git.projectsss@gmail.com>
2023-09-22 13:39:39 +0200

    Add listeners

--- form.js
+++ form.js
@@ -13,17 +13,29 @@ function appSubEl(parent, child){
   return parent.appendChild(child);
 }
 
+function getEl(target){
+  return document.getElementById(target).value;
+}
+
+function onSubmit(e){
+  e.preventDefault();
+  console.log(getEl("email"));
+  console.log(getEl("password"));
+}
 const inputEmail = createNewEl("input");
 createNewAttr(inputEmail, "type", "text");
+createNewAttr(inputEmail, "id", "email");
 createNewAttr(inputEmail, "placeholder", "Enter your email");
 
 const inputPassword = createNewEl("input");
 createNewAttr(inputPassword, "type", "text");
+createNewAttr(inputPassword, "id", "password");
 createNewAttr(inputPassword, "placeholder", "Enter your password");
 
 const btnSub = createNewEl("button");
 createNewAttr(btnSub, "type", "submit");
 btnSub.textContent = "Submit";
+btnSub.addEventListener("click", onSubmit);
 
 appSubEl(form, inputEmail);
 appSubEl(form, inputPassword);
