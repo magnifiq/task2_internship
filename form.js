e358f4f3f7072b8fd4313454751c68e5660409f7
magnifiq <git.projectsss@gmail.com>
2023-09-25 11:15:01 +0200

    Post request

--- form.js
+++ form.js
@@ -19,7 +19,7 @@ const getElementValue=(target)=>{
   return document.getElementById(target).value;
 }
 
-const onSubmit=(e)=>{
+const onSubmit=async(e)=>{
   e.preventDefault();
   const emailValue = getElementValue("email");
   const passwordValue = getElementValue("password");
@@ -29,9 +29,33 @@ const onSubmit=(e)=>{
     return
   }else if(passwordValue.trim()==''&&passwordValue.length<5){
     console.error("Please, enter the valid password")
+    return
   }else{
-    console.log(getElementValue("email"));
-    console.log(getElementValue("password"));
+    console.log(emailValue);
+    console.log(passwordValue);
+
+    const data={
+      emailValue,
+      passwordValue
+    }
+
+    const options={
+      method: "POST",
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body:JSON.stringify(data),
+    }
+    try{
+      const response=await fetch('https://httpbin.org/post', options);
+
+      if(response.ok){
+        const responseData=await response.json()
+        console.log("Sent Data:", responseData.json);
+      }
+    }catch{
+      console.error("Something goes wrong")
+    }
 
     const password=createNewEl('div');
     const email=createNewEl('div');
