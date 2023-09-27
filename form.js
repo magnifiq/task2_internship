e666482be417f31ef023c72c9587f179fc444092
magnifiq <git.projectsss@gmail.com>
2023-09-27 20:50:08 +0200

    version 3.0

--- form.js
+++ form.js
@@ -1,73 +1,71 @@
 const root = document.getElementById("root");
 
-const createNewEl=(el)=>{
+const createNewEl = (el) => {
   return document.createElement(el);
-}
-const form = createNewEl('form');
+};
+const form = createNewEl("form");
 
-const addNewAttr=(el, attr, val)=>{
+const addNewAttr = (el, attr, val) => {
   return el.setAttribute(attr, val);
-}
+};
 
-const appendSubEl=(parent, ...rest)=>{
-  rest.forEach(child=>{
-    parent.appendChild(child)
-  })
-}
+const appendSubEl = (parent, ...rest) => {
+  rest.forEach((child) => {
+    parent.appendChild(child);
+  });
+};
 
-const getElementValue=(target)=>{
+const getElementValue = (target) => {
   return document.getElementById(target).value;
-}
+};
 
-const onSubmit=async(e)=>{
+const onSubmit = async (e) => {
   e.preventDefault();
   const emailValue = getElementValue("email");
   const passwordValue = getElementValue("password");
 
-  if(emailValue.trim()==''){
+  if (emailValue.trim() == "") {
     console.error("Please, enter the email");
-    return
-  }else if(passwordValue.trim()==''&&passwordValue.length<5){
-    console.error("Please, enter the valid password")
-    return
-  }else{
+    return;
+  } else if (passwordValue.trim() == "" && passwordValue.length < 5) {
+    console.error("Please, enter the valid password");
+    return;
+  } else {
     console.log(emailValue);
     console.log(passwordValue);
 
-    const data={
+    const data = {
       emailValue,
-      passwordValue
-    }
+      passwordValue,
+    };
 
-    const options={
+    const options = {
       method: "POST",
-      headers:{
-        'Content-Type':'application/json',
+      headers: {
+        "Content-Type": "application/json",
       },
-      body:JSON.stringify(data),
-    }
-    try{
-      const response=await fetch('https://httpbin.org/post', options);
+      body: JSON.stringify(data),
+    };
+    try {
+      const response = await fetch("https://httpbin.org/post", options);
 
-      if(response.ok){
-        const responseData=await response.json()
+      if (response.ok) {
+        const responseData = await response.json();
         console.log("Sent Data:", responseData.json);
-      }
-    }catch{
-      console.error("Something goes wrong")
-    }
+      };
+    } catch {
+      console.error("Something goes wrong");
+    };
 
-    const password=createNewEl('div');
-    const email=createNewEl('div');
-    appendSubEl(root, email,password)
-    password.textContent=getElementValue("email");
-    email.textContent=getElementValue("password")
+    const password = createNewEl("div");
+    const email = createNewEl("div");
+    appendSubEl(root, email, password);
+    password.textContent = getElementValue("email");
+    email.textContent = getElementValue("password");
 
-    document.getElementsByTagName('form')[0].reset()
-  }
-
-  
-}
+    document.getElementsByTagName("form")[0].reset();
+  };
+};
 const inputEmail = createNewEl("input");
 addNewAttr(inputEmail, "type", "text");
 addNewAttr(inputEmail, "id", "email");
